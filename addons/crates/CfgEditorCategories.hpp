42f1562d430a8e58d627acdf0e953e5accd75749
Ferdilanz <100645597+Ferdilanz@users.noreply.github.com>
2023-09-30 14:41:45 -0400

    Add files via upload
    added CfgEditorCategories, update CfgVehicles, and update config.cpp

---
+++ addons/crates/CfgEditorCategories.hpp
@@ -0,0 +1,38 @@
+class CfgEditorCategories
+{
+	class EverlightCrates
+	{
+		displayName = "Everlight Crates"; 
+	};
+};
+class CfgEditorSubcategories
+{
+	class FT_crates
+	{
+		displayName = "FT Crates";
+	};
+	class AR_crates
+	{
+		displayName = "AR Crates";
+	};
+	class MMG_crates
+	{
+		displayName = "MMG Crates";
+	};
+	class RAT_crates
+	{
+		displayName = "RAT Crates";
+	};
+	class LAT_crates
+	{
+		displayName = "LAT Crates";
+	};
+	class MAT_crates
+	{
+		displayName = "MAT Crates";
+	};
+	class HAT_crates
+	{
+		displayName = "HAT Crates";
+	};
+};
