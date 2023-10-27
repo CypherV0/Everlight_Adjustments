e17e21bdafb59e5b55af1293affa147b6b321804
Ferdilanz <100645597+Ferdilanz@users.noreply.github.com>
2023-10-27 13:19:02 -0400

    Added Logistics Crates, removed Empty Crates

--- addons/crates/CfgEditorCategories.hpp
+++ addons/crates/CfgEditorCategories.hpp
@@ -43,8 +43,8 @@ class CfgEditorSubcategories
 	{
 		displayName = "AA Missile Crates";
 	};
-	class Empty_crates
+	class Logi_crates
 	{
-		displayName = "Empty Crates"
+		displayName = "Logistics Crates";
 	};
 };
